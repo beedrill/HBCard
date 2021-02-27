d729b85d00ce96596e5125a9eae293f9185c2baa
Rusheng <zrs1990@gmail.com>
2021-02-26 21:40:39 -0500

    add 2line support for signature and number of games 1-4

--- src/components/DetailForm.vue
+++ src/components/DetailForm.vue
@@ -15,6 +15,8 @@
         <q-input class="q-my-sm" outlined v-model="name" label="昵称" />
          <q-input class="q-my-sm" outlined v-model="voice" label="音色(例如：少女音，萝莉音，御姐音)" />
          <q-input class="q-my-sm" outlined v-model="signature" label="个人签名（彰显你的特点）" />
+         <q-input v-if="longSignature" class="q-my-sm" outlined v-model="secondLineSignature" label="第二行签名" />
+         <div><q-toggle color="deep-orange" label="长签名(可以写两行签名)" v-model="longSignature"></q-toggle></div>
          陪玩项目：
           <q-select
             filled
@@ -23,7 +25,7 @@
             :options="gameOptions"
             counter
             max-values="4"
-            hint="必须选4个"
+            hint="最多选4个"
             style="width: 250px"
         />
         <q-btn v-if="debug" label="预览" @click="preview"/>
@@ -39,14 +41,16 @@ export default {
   data () {
     return {
       debug: false,
+      longSignature: false,
       games: [],
       gameOptions: [
-        '英雄联盟', 'PUBG', 'TFT', 'COD', 'CSGO', '彩虹6号', '逃离塔科夫', 'Steam小游戏', 'DOTA2', 'Valorant', '守望先锋', 'Apex', '手游', '魔兽', '其他端游', '陪聊', '唱歌'
+        '英雄联盟', 'PUBG', 'TFT', 'COD', 'CSGO', '彩虹6号', '逃离塔科夫', 'Steam', 'DOTA2', 'Valorant', '守望先锋', 'Apex', '手游', '魔兽', '其他端游', '陪聊', '唱歌'
       ],
       voice: '',
       signature: '',
       imageUrl: '',
-      name: ''
+      name: '',
+      secondLineSignature: ''
     }
   },
   mounted () {
@@ -63,7 +67,9 @@ export default {
         games: this.games,
         voice: this.voice,
         signature: this.signature,
-        avatar: imageUrl
+        secondLineSignature: this.secondLineSignature,
+        avatar: imageUrl,
+        longSignature: this.longSignature
       }
     },
     preview () {
--- src/components/NamecardCanvas.vue
+++ src/components/NamecardCanvas.vue
@@ -67,15 +67,28 @@ export default {
       if (!data.voice) {
         return '没有音色'
       }
-      if (data.games.length !== 4) {
+      if (data.games.length > 4) {
         console.log(data.games.length)
-        return '游戏项目必须是4个'
+        return '游戏项目最多4个'
+      }
+      if (data.games.length === 0) {
+        console.log(data.games.length)
+        return '至少需要一个游戏'
       }
       if (!this.checkNameStyle(data.name)) {
         return '你输入的姓名过长，请更换一个短一些的昵称'
       }
-      if (!this.checkSignatureStyle(data.signature)) {
-        return '你输入的签名过长，请更换一个短一些的'
+      if (data.longSignature) {
+        if (!this.checkLongSignatureStyle(data.signature)) {
+          return '第一行签名过长'
+        }
+        if (!this.checkLongSignatureStyle(data.secondLineSignature)) {
+          return '第二行签名过长'
+        }
+      } else {
+        if (!this.checkSignatureStyle(data.signature)) {
+          return '你输入的签名过长，请更换一个短一些的'
+        }
       }
       if (!this.checkVoiceStyle(data.voice)) {
         return '你输入的音色过长，请更换一个短一些的'
@@ -118,7 +131,23 @@ export default {
       ctx.font = '35px font230'
       return ctx.measureText(signature).width < 400
     },
+    checkLongSignatureStyle (signature) {
+      var ctx = this.canvas.getContext('2d')
+      ctx.font = '30px font230'
+      return ctx.measureText(signature).width < 400
+    },
+    drawLongSignature (data) {
+      var ctx = this.canvas.getContext('2d')
+      ctx.textAlign = 'start'
+      ctx.font = '30px font230'
+      ctx.fillText(data.signature, 25, 210)
+      ctx.fillText(data.secondLineSignature, 400 - ctx.measureText(data.secondLineSignature).width, 250)
+    },
     drawSignature (data) {
+      if (data.longSignature) {
+        this.drawLongSignature(data)
+        return
+      }
       var ctx = this.canvas.getContext('2d')
       const x = this.canvas.width / 2
       ctx.textAlign = 'center'
@@ -126,6 +155,60 @@ export default {
       ctx.fillText(data.signature, x, 240)
     },
     drawGame (data) {
+      if (data.games.length === 4) {
+        this.drawGame4(data)
+      }
+      if (data.games.length === 3) {
+        this.drawGame3(data)
+      }
+      if (data.games.length === 2) {
+        this.drawGame2(data)
+      }
+      if (data.games.length === 1) {
+        this.drawGame1(data)
+      }
+    },
+    drawGame1 (data) {
+      var ctx = this.canvas.getContext('2d')
+      ctx.font = '42px font143'
+      if (this.type === 'M') {
+        ctx.fillStyle = '#A6BBCF'
+      } else {
+        ctx.fillStyle = '#FFD1DC'
+      }
+      ctx.textAlign = 'center'
+      ctx.fillText(data.games[0], 110, 400)
+    },
+    drawGame2 (data) {
+      var ctx = this.canvas.getContext('2d')
+      ctx.font = '42px font143'
+      if (this.type === 'M') {
+        ctx.fillStyle = '#A6BBCF'
+      } else {
+        ctx.fillStyle = '#FFD1DC'
+      }
+      ctx.textAlign = 'start'
+      ctx.fillText(data.games[0], 20, 360)
+      ctx.fillStyle = '#B2B2B2'
+      ctx.font = '34px font143'
+      ctx.fillText(data.games[1], 500 - 270 - ctx.measureText(data.games[1]).width, 420)
+    },
+    drawGame3 (data) {
+      var ctx = this.canvas.getContext('2d')
+      ctx.font = '38px font143'
+      if (this.type === 'M') {
+        ctx.fillStyle = '#A6BBCF'
+      } else {
+        ctx.fillStyle = '#FFD1DC'
+      }
+      ctx.textAlign = 'start'
+      ctx.fillText(data.games[0], 500 - 270 - ctx.measureText(data.games[0]).width, 300)
+      ctx.fillStyle = '#B2B2B2'
+      ctx.font = '34px font143'
+      ctx.fillText(data.games[1], 500 - 270 - ctx.measureText(data.games[1]).width, 370)
+      ctx.fillText(data.games[2], 500 - 270 - ctx.measureText(data.games[2]).width, 440)
+    },
+    drawGame4 (data) {
       var ctx = this.canvas.getContext('2d')
       ctx.font = '34px font143'
       if (this.type === 'M') {
